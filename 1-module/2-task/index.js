728646744c184b6488a88e24c57c77fb7bc36390
Poldyaich97 <daniil.poldyaev97@gmail.com>
2024-04-24 15:48:18 +0500

    Решение задач 2,3,4

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,15 +10,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name !== null && name.length >= 4 && name.indexOf(" ") === -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
