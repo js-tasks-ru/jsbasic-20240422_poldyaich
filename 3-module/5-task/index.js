67dd1cedc95b393258818a0dd7cc76eecb1a730b
Poldyaich97 <daniil.poldyaev97@gmail.com>
2024-05-05 19:53:45 +0500

    3 модуль

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str
+    .split(" ")
+    .map((item) => parseFloat(item))
+    .filter((item) => !isNaN(item));
+  let min = Math.min(...numbers);
+  let max = Math.max(...numbers);
+  return { min, max };
 }
