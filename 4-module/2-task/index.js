aa04713f00bd7631ab651afecb69800897275919
Poldyaich97 <daniil.poldyaev97@gmail.com>
2024-05-13 10:03:43 +0500

    4 модуль

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rowCount = table.rows.length;
+  let colCount = table.rows[0].cells.length;
+  for (let i = 0; i < Math.min(rowCount, colCount); i++) {
+    let cell = table.rows[i].cells[i];
+    cell.style.backgroundColor = "red";
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.querySelectorAll("tbody tr");
+  rows.forEach((row) => {
+    let statusCell = row.querySelector("td[data-available]");
+    let genderCell = row.querySelector("td:nth-child(3)");
+    let ageCell = row.querySelector("td:nth-child(2)");
+
+    if (statusCell) {
+      if (statusCell.dataset.available === "true") {
+        row.classList.add("available");
+      } else if (statusCell.dataset.available === "false") {
+        row.classList.add("unavailable");
+      }
+    } else {
+      row.hidden = true;
+    }
+
+    if (genderCell) {
+      if (genderCell.textContent === "m") {
+        row.classList.add("male");
+      } else if (genderCell.textContent === "f") {
+        row.classList.add("female");
+      }
+    }
+
+    if (ageCell) {
+      if (parseInt(ageCell.textContent) < 18) {
+        row.style.textDecoration = "line-through";
+      }
+    }
+  });
 }
